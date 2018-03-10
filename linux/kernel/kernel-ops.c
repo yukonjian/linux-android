3648fa4bdaa1cd8f15ff473f77dde7edf1ade2ce
yukonjian <yukonjian@126.com>
2018-03-10 20:37:34 +0800

    a

--- linux/kernel/kernel-ops.c
+++ linux/kernel/kernel-ops.c
@@ -6,3 +6,27 @@ if (!port)
 kfree(port);
 
 2.
+错误判断函数
+在linux/err.h中包含了这一机制的处理，主要通过IS_ERR, PTR_ERR, ERR_PTR几个宏。
+#define MAX_ERRNO       4095
+#define IS_ERR_VALUE(x) unlikely((x) >= (unsigned long)-MAX_ERRNO)
+/* 将错误号转化为指针，由于错误号在-1000~0间，返回的指针会落在最后一页  */
+static inline void *ERR_PTR(long error)
+{
+         return (void *) error;
+}
+/* 将指针转化为错误号  */
+static inline long PTR_ERR(const void *ptr)
+{
+         return (long) ptr;
+}
+/* 判断返回的指针是错误信息还是实际地址，即指针是否落在最后一页 */
+static inline long IS_ERR(const void *ptr)
+{
+         return IS_ERR_VALUE((unsigned long)ptr);
+}
+对于内核中返回的指针，检查错误的方式不是if(!retptr)，而是if( IS_ERR(retptr) 或 If( IS_ERR_VALUE(retptr) )。
+static inline long __must_check IS_ERR_OR_NULL(const void *ptr)
+{
+       return !ptr || IS_ERR_VALUE((unsigned long)ptr);
+}
