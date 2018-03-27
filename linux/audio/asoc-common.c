ecbb71e3406f50057cba076c5920b7341828ba17
yukonjian <yukonjian@126.com>
2018-03-27 22:41:03 +0800

    1

---
+++ linux/audio/asoc-common.c
@@ -0,0 +1,5 @@
+1.DAPM : Dynamic Audio Power Management
+2.Linux-ASoC驱动归纳总结：
+https://blog.csdn.net/sunruichen/article/details/8568273
+3.ALSA主页
+http://www.alsa-project.org/main/index.php/Main_Page
