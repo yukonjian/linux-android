eca9736fedc155a17fead7c968f6e9aa3e1000f5
yukonjian <yukonjian@126.com>
2018-07-26 21:57:03 +0800

    1

--- linuxnew/chardev/GPIO子系统/gpio_base.c
+++ linuxnew/chardev/GPIO子系统/gpio_base.c
@@ -1,5 +1,6 @@
 1. gpio子系统的核心文件
 /driver/gpio/gpiolib.c
+参考文件：/Documentation/gpio.txt
 
 2. gpio的使用
 1) 判断gpio num是否可以使用
@@ -14,9 +15,12 @@ return: 成功：0
 
 3) 设置gpio的输入还是输出
 int gpio_direction_input(unsigned gpio)
-static int gpiod_direction_input(struct gpio_desc *desc)
+gpiod_direction_output(struct gpio_desc *desc)
 return：成功：0
 
 4) 得到gpio的值和设置gpio的值
 int gpio_get_value(unsigned int gpio)
 void gpio_set_value(unsigned int gpio, int value)
+
+5) 释放申请的gpio引脚
+void gpio_free(unsigned gpio)
