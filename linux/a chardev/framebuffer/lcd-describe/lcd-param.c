cc174522b402fe83bf32baaf69fe1ca5bdd3fb4c
yukonjian <yukonjian@126.com>
2018-12-27 22:55:07 +0800

    1

--- linux/a chardev/framebuffer/lcd-describe/lcd-param.c
+++ linux/a chardev/framebuffer/lcd-describe/lcd-param.c
@@ -6,3 +6,9 @@ vfp              /* Vertical front porch */
 vbp              /* Vertical back porch */
 vsw              /* Vertical Sync Pulse Width */
 bpp              /* Bits per pixel */
+
+https://blog.csdn.net/u010781856/article/details/42177409
+
+VSYNC, HSYNC,DCLK,VDEN 的极性(0--normal，1---inverted)
+VSYNC,HSYNC,VDEN电平脉冲为高电平（normal）
+DCLK上升沿发送数据，下降延采样（normal）
