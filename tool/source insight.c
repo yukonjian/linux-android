6559da27d5991a88085efac1b87863eb20f77eb0
yukonjian <yukonjian@126.com>
2019-03-01 14:38:02 +0800

    1

---
+++ tool/source insight.c
@@ -0,0 +1,36 @@
+1. 常用快捷键
+F3　：本文件查找结果的上一个
+F4　：本文件查找结果的下一个
+F7	：打开Browse Project Symbols窗口，快速浏览工程内标识定义
+F8	:高亮选中的字
+“Ctrl+=” 或 Ctrl+鼠标点击标识	：直接跳转至标识定义处调用处
+Ctrl+M　：创建或查找书签，方便下次找回此位置
+2. 常用功能
+1）括号配对高亮：
+“在前括号左侧，后括号左侧” 双击鼠标左键，可以选定匹配括号和其中内容(<>,(),L{R},[]之间)
+
+Source Insight常用的快捷键：
+
+Ctrl+= :Jump to definition
+Alt+/ :Look up reference
+F3 : search backward
+F4 : search forward
+F5: go to Line
+F7 :Look up symbols
+F8 :Look up local symbols
+F9 :Ident left
+F10 :Ident right
+Alt+, :Jump backword
+Alt+. : Jump forward
+Shift+F3 : search the word under cusor backward
+Shift+F4 : search the word under cusor forward
+F12 : incremental search
+Shift+Ctrl+f: search in project
+shift+F8 : hilight word
+
+
+窗口操作:
+project window Ctrl+O打开
+symbol window Alt+F8打开和关闭
+Contex Window 自定义键打开和关闭
+Relation Window 自定义键打开　先锁定再刷新联系
