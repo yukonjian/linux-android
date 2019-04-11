e15f1dc37f759ee05c11341e89cf2a0821d83c08
yukonjian <yukonjian@126.com>
2019-04-11 21:55:40 +0800

    1

--- linux/a chardev/b charbase/b file-ops/mmap.c
+++ linux/a chardev/b charbase/b file-ops/mmap.c
@@ -9,6 +9,30 @@ struct vm_area_struct {
   pgprot_t vm_page_prot;		/* vma的访问权限 */
   unsigned long vm_flags;    /* 标识集 */
 
-  unsigned long vm_pgoff;		/* 映射文件的偏移量，以PAGE_SIZE为单位 */
+  unsigned long vm_pgoff;		/* 映射文件的偏移量,相对于映射的物理地址开始的偏移量，以PAGE_SIZE为单位 */
   struct file * vm_file;		    /* 映射的文件，没有则为NULL */
 }
+
+static int remap_pfn_mmap(struct file *file, struct vm_area_struct *vma)
+{
+    unsigned long offset = vma->vm_pgoff << PAGE_SHIFT;
+    unsigned long pfn_start = (virt_to_phys(kbuff) >> PAGE_SHIFT) + vma->vm_pgoff;
+    unsigned long virt_start = (unsigned long)kbuff + offset;
+    unsigned long size = vma->vm_end - vma->vm_start;
+    int ret = 0;
+
+    printk("phy: 0x%lx, offset: 0x%lx, size: 0x%lx\n", pfn_start << PAGE_SHIFT, offset, size);
+
+    ret = remap_pfn_range(vma, vma->vm_start, pfn_start, size, vma->vm_page_prot);
+    if (ret)
+        printk("%s: remap_pfn_range failed at [0x%lx  0x%lx]\n",
+            __func__, vma->vm_start, vma->vm_end);
+    else
+        printk("%s: map 0x%lx to 0x%lx, size: 0x%lx\n", __func__, virt_start,
+            vma->vm_start, size);
+
+    return ret;
+}
+remap_pfn_range将物理页帧号pfn_start对应的物理内存映射到用户空间的vm->vm_start处，映射长度为该虚拟内存区的长度。
+由于这里的内核缓冲区是用kzalloc分配的，保证了物理地址的连续性，
+所以会将物理页帧号从pfn_start开始的（size >> PAGE_SHIFT）个连续的物理页帧依次按序映射到用户空间。
