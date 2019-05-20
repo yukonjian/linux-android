957b7df758382115b7f183f49bd5c6681b15d457
yukonjian <yukonjian@126.com>
2019-05-20 18:45:12 +0800

    1

--- android/d tinyalsa/tinyalsa_base.c
+++ android/d tinyalsa/tinyalsa_base.c
@@ -4,7 +4,7 @@ cat /proc/asound/cards
 /external/tinyalsa
 3. tinycap录音
 Usage: tinycap file.wav [-D card] [-d device] [-c channels] [-r rate] [-b bits] [-p period_size] [-n n_periods]
-note: 在使用 file.wav 要放在最前面。
+note: 在使用时； file.wav 要放在最前面。
 4. tinyplay播放
 Usage: tinyplay file.wav [-D card] [-d device] [-p period_size] [-n n_periods]
 5. tinymix查看混音
---
+++ linux/network/sample/ssdk.c
@@ -0,0 +1,3 @@
+1. athrs_mac.c / athr_gmac_init();
+1.1 athr_gmac_phy_attach(mac,mac->mac_unit);
+1.1.1 qthrs17_phy.c / int athrs17_register_ops(void *arg)
