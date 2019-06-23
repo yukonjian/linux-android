059dc1cbf6dd59f39e32936e06b0f785181c4b0f
yukonjian <yukonjian@126.com>
2019-06-23 17:58:17 +0800

    2

--- linux/o command/linux-command.c
+++ linux/o command/linux-command.c
@@ -221,3 +221,9 @@ root:x:1000:1000:Linux User,,,:/home/root:/bin/sh
 root:x:0:0:Linux User,,,:/home/root:/bin/sh
 
 30. ls -lh //可以查看当前文件大小，使用k,M为单位进行显示
+
+31. cut 
+cut是切割命令，参数-d表示按什么划分，参数-f表示截取哪些字段。
+例如: env | grep "USER" | cut -d "=" -f 2;
+env | grep "USER"执行结果为：USER=root
+cut -d "=" -f 2   表示将其=号作为分隔符，选取其第2个字段，所有就是root
--- ubuntu/静态ip设置.c
+++
@@ -1,6 +0,0 @@
-1. 静态ip设置
-1) 编辑配置文件/etc/network/interfaces
-增加以下内容：
-2) 配置dns，编辑配置文件/etc/resolv.conf，以访问外网
-增加如下内容：
-nameserver 114.114.114.114
