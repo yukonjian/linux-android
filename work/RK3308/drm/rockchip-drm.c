90c18f2cc337feda4559ec4b72851244b197c466
yukonjian <yukonjian@126.com>
2019-10-12 16:01:27 +0800

    2

---
+++ work/RK3308/drm/rockchip-drm.c
@@ -0,0 +1,14 @@
+设备树兼容属性                 包含属性的文件路径
+rockchip,display-subsystem   \kernel\drivers\gpu\drm\rockchip\rockchip_drm_drv.c
+
+
+Linux DRM Graphic 显示简单介绍
+https://blog.csdn.net/yangkuanqaz85988/article/details/48657615
+Linux DRM KMS 驱动简介
+https://blog.csdn.net/yangkuanqaz85988/article/details/48689521
+
+Linux DRM Developer's Guide
+http://www.landley.net/kdocs/htmldocs/drm.html#idp3791168
+
+kernel - component组件用法
+https://blog.csdn.net/qq_14997637/article/details/100977536
